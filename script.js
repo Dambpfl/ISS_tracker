b164f57c25c7b224729c533262464a8d99a5f99e
Damien <boenapfel.damien@yahoo.fr>
2026-08-27 18:02:48 +0200

    FIX : Marker map

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let issIcon = L.icon({
-    iconUrl: '/images/iss.png',
+    iconUrl: './images/iss.png',
     shadowUrl: null,
 
     iconSize:     [100, 100], // size of the icon
