24ed616f240f39e6d03e4bf67ab0dfc1af3196df
Damien <boenapfel.damien@yahoo.fr>
2026-09-01 15:32:58 +0200

    Add: icone + changement design navbar

--- script.js
+++ script.js
@@ -12,39 +12,39 @@ let issIcon = L.icon({
     iconUrl: './images/iss.png',
     shadowUrl: null,
 
-    iconSize:     [100, 100], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [50, 50], // point of the icon which will correspond to marker's location
+    iconSize: [100, 100], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [50, 50], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
 
 const issPositions = [];
 const issPath = L.polyline(issPositions, { color: 'red' }).addTo(map);
-const issMarker = L.marker([0, 0], {icon: issIcon}).addTo(map);
+const issMarker = L.marker([0, 0], { icon: issIcon }).addTo(map);
 
 setInterval(() => {
     fetch('https://api.wheretheiss.at/v1/satellites/25544')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-        const latitude = data.latitude
-        const longitude = data.longitude
-    
-        issMarker.setLatLng([latitude, longitude])
-        issPositions.push([latitude, longitude])
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            const latitude = data.latitude
+            const longitude = data.longitude
 
-        if (issPositions.length > 500) {
-            issPositions.shift()
-        }
+            issMarker.setLatLng([latitude, longitude])
+            issPositions.push([latitude, longitude])
 
-        issPath.setLatLngs(issPositions)
-    })
+            if (issPositions.length > 500) {
+                issPositions.shift()
+            }
 
-    .catch((error) => {
-    console.error("Erreur: ", error);
-    })
+            issPath.setLatLngs(issPositions)
+        })
+
+        .catch((error) => {
+            console.error("Erreur: ", error);
+        })
 
 }, 1000)
 
